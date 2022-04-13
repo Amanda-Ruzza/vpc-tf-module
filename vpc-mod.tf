c4552ff3666bf5adde549463a7d52a9baed7c21d
Amanda-Ruzza <amanda.ruzza@cloudreach.com>
2022-04-12 20:40:20 -0400

    Renamed the VPC file

--- vpc-mod.tf
+++ vpc-mod.tf
@@ -2,59 +2,7 @@ resource "aws_vpc" "main" {
   cidr_block = "192.168.0.0/16"
 
   tags = {
-    Name = "talent-academy-vpc"
-  }
-}
-
-resource "aws_subnet" "public_a" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "192.168.1.0/24"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "talent-academy-public-a"
-  }
-}
-
-resource "aws_subnet" "public_b" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "192.168.10.0/24"
-  availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "talent-academy-public-b"
-  }
-}
-
-resource "aws_subnet" "private_a" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "192.168.2.0/24"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "talent-academy-private-a"
-  }
-}
-
-resource "aws_subnet" "private_b" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "192.168.20.0/24"
-  availability_zone = "us-east-1b"
-
-  tags = {
-    Name = "talent-academy-private-b"
-  }
-}
-
-resource "aws_subnet" "app" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "192.168.3.0/24"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "talent-academy-app-a"
+    Name = "${var.project_name}-vpc"
   }
 }
 
@@ -62,7 +10,7 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "talent-academy-igw"
+    Name = "${var.project_name}-igw"
   }
 }
 
@@ -76,9 +24,11 @@ resource "aws_nat_gateway" "nat_gw" {
   subnet_id         = aws_subnet.public.id
 
   tags = {
-    Name = "talent-academy-nat-gw"
+    Name = "${var.project_name}-nat-gw"
 
   }
 
   depends_on = [aws_internet_gateway.igw]
 }
+
+# Separated the Subnets from this VPC files
